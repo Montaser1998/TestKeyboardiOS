2230bd11f6b3a5a1697b4db857e7d65aa3c6d864
montaser1998 <montaser268@gmail.com>
2020-05-05 16:54:21 +0300

    Add project files.

---
+++ TestKeyboardiOS/TestKeyboardiOS.iOS/CustomEditorRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using TestKeyboardiOS.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(CustomEditorRenderer))]
+namespace TestKeyboardiOS.iOS
+{
+    public class CustomEditorRenderer : EditorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null && Element == null) return;
+
+            Control.AutoresizingMask = UIViewAutoresizing.All;
+        }
+    }
+}

d987c00aa6cc9a8658fad7f7e5fac1862fa7f7e8
montaser1998 <montaser268@gmail.com>
2020-05-05 17:07:13 +0300

    Fix keyboard

--- TestKeyboardiOS/TestKeyboardiOS.iOS/CustomEditorRenderer.cs
+++ TestKeyboardiOS/TestKeyboardiOS.iOS/CustomEditorRenderer.cs
@@ -2,25 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using CoreGraphics;
 using Foundation;
 using TestKeyboardiOS.iOS;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
-[assembly: ExportRenderer(typeof(Editor), typeof(CustomEditorRenderer))]
+[assembly: ExportRenderer(typeof(TestKeyboardiOS.KeyboardView), typeof(KeyboardViewRenderer))]
 namespace TestKeyboardiOS.iOS
 {
-    public class CustomEditorRenderer : EditorRenderer
+    public class KeyboardViewRenderer : ViewRenderer
     {
-        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        NSObject _keyboardShowObserver;
+        NSObject _keyboardHideObserver;
+        protected override void OnElementChanged(ElementChangedEventArgs<View> e)
         {
             base.OnElementChanged(e);
 
-            if (Control == null && Element == null) return;
+            if (e.NewElement != null)
+            {
+                RegisterForKeyboardNotifications();
+            }
 
-            Control.AutoresizingMask = UIViewAutoresizing.All;
+            if (e.OldElement != null)
+            {
+                UnregisterForKeyboardNotifications();
+            }
+        }
+
+        void RegisterForKeyboardNotifications()
+        {
+            if (_keyboardShowObserver == null)
+                _keyboardShowObserver = UIKeyboard.Notifications.ObserveWillShow(OnKeyboardShow);
+            if (_keyboardHideObserver == null)
+                _keyboardHideObserver = UIKeyboard.Notifications.ObserveWillHide(OnKeyboardHide);
+        }
+
+        void OnKeyboardShow(object sender, UIKeyboardEventArgs args)
+        {
+
+            NSValue result = (NSValue)args.Notification.UserInfo.ObjectForKey(new NSString(UIKeyboard.FrameEndUserInfoKey));
+            CGSize keyboardSize = result.RectangleFValue.Size;
+            if (Element != null)
+            {
+                Element.Margin = new Thickness(0, 0, 0, keyboardSize.Height); //push the entry up to keyboard height when keyboard is activated
+
+            }
+        }
+
+        void OnKeyboardHide(object sender, UIKeyboardEventArgs args)
+        {
+            if (Element != null)
+            {
+                Element.Margin = new Thickness(0); //set the margins to zero when keyboard is dismissed
+            }
+
+        }
+
+
+        void UnregisterForKeyboardNotifications()
+        {
+            if (_keyboardShowObserver != null)
+            {
+                _keyboardShowObserver.Dispose();
+                _keyboardShowObserver = null;
+            }
+
+            if (_keyboardHideObserver != null)
+            {
+                _keyboardHideObserver.Dispose();
+                _keyboardHideObserver = null;
+            }
         }
     }
 }
